0d05e4fd42330bcc73dd3902923a6ddcf841a0b5
rupeena0805 <rupeenakumra@gmail.com>
2025-07-29 21:53:52 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 let mongoose = require('mongoose')
-mongodb+srv://rupeena:Rupeena0805@cluster0.gvovnaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-const dbPath = 'mongodb+srv://dbuser:dbpassword@mongo.vk6tb.mongodb.net/kizaTexttiles'
+const dbPath = 'mongodb+srv://rupeena:Rupeena0805@cluster0.gvovnaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
     console.log("Db Connected")
