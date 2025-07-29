a925213cddd713a8fffaa4b0b5c605bb6a12987a
rupeena0805 <rupeenakumra@gmail.com>
2025-07-29 21:28:50 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-mongodb+srv://rupeena:Rupeens0805@cluster0.gvovnaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongodb+srv://rupeena:Rupeena0805@cluster0.gvovnaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const dbPath = 'mongodb+srv://dbuser:dbpassword@mongo.vk6tb.mongodb.net/kizaTexttiles'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
